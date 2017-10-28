62f6c0f73f3c64c860b05ffcbed71d9075b220cb
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2017-10-28 17:29:05 -0400

    creating and setting up server file

---
+++ server.js
@@ -0,0 +1,39 @@
+// Dependences
+const express = require('express');
+const logger = require('morgan');
+const Path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+// Port for listening
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Logger
+app.use(logger('dev'));
+
+// Static file path
+app.use(express.static('publice'));
+
+// For Heroku Build up
+// (process.env.NODE_ENV === 'production')
+// app.use(express.static('client/build'));
+
+// Routes
+const skyRoute = require('./routes/skyroutes');
+app.use('/api', skyRoute);
+
+// Error message
+app.get('*', (req, res) => {
+    res.status(404).json({message: 'Not Found'});
+})
