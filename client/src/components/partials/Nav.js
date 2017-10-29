1852ebd2be60ac49570dffaab29c8ee3493e0a3b
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2017-10-29 15:24:52 -0400

    date added nav

---
+++ client/src/components/partials/Nav.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class nav extends Component {
+    constructor() {
+        super();
+        this.state = {
+          date: null,
+        };
+        this.navDate = this.navDate.bind(this);
+      }
+    
+      navDate() {
+        let date = new Date();
+        let days = ["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"];
+        let months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
+        return `${days[date.getDay()]}  -  ${months[date.getMonth()]} ${date.getDate()}`
+      }
+  render() {
+    return (
+        <header>
+        <i className="fa fa-bars fa-lg colorMenu" aria-hidden="true"></i>
+        <span className="navDate ">{this.navDate()}</span>
+        </header>
+    )
+  }
+}
+
+export default nav;
