391f885aa373c261ebae7a7ccb55c91953167814
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2017-10-29 01:44:11 -0400

    front home page added

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class home extends Component {
+  render() {
+    return (
+      <div className="homeContainer">
+      <div className="logo"></div>
+      <span className="homeText">Wana know if you'll melt outside?</span>
+      <span className="homeText">Start getting some weather.</span>
+      </div>
+    );
+  }
+}
+
+export default home;
